3b99b94171e7907c708edbe9e8d5ea088556bcb1
Eldar Abusalimov <eldar.abusalimov@jetbrains.com>
2017-08-07 21:52:49 +0300

    build: Don't use packman, extract packages manually

--- build.sh
+++ build.sh
@@ -4,11 +4,22 @@
 # Author: Renato Silva <br.renatosilva@gmail.com>
 # Author: Qian Hong <fracting@gmail.com>
 
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 makepkg.conf package..." >&2
+    exit 1
+fi
+
+MAKEPKG_CONF="${1}"; shift
+if [ ! -f "${MAKEPKG_CONF}" ]; then
+    echo "${MAKEPKG_CONF}: No such file" >&2
+    exit 1
+fi
+
 # Configure
-cd "$(dirname "$0")"
-source 'libbuild.sh'
-source 'makepkg-mingw64.conf'
-deploy_enabled && mkdir -p artifacts
+DIR="$(dirname "$0")"
+source "${DIR}/libbuild.sh"
+source "${MAKEPKG_CONF}"
+deploy_enabled && [ -d artifacts ] || mkdir -p artifacts
 # git_config user.email 'ci@msys2.org'
 # git_config user.name  'MSYS2 Continuous Integration'
 git config --global user.email 'Eldar.Abusalimov@jetbrains.com'
@@ -23,19 +34,19 @@ git config --global user.name  'Eldar Abusalimov'
 test -z "$@" && failure 'No packages specified'
 define_build_order "$@" || failure 'Could not determine build order'
 
-export PACMAN=~/pacman-wrapper
-mkdir -p ~/pacman/{db,root,cache}
+# mkdir -p ~/pacman/{db,root,cache}
 
-execute 'Generating pacman repository' repo-add ~/pacman/db/repo.db.tar.xz
-execute 'Syncing pacman repository' $PACMAN -Syy
+# execute 'Generating pacman repository' repo-add ~/pacman/db/repo.db.tar.xz
+export PACMAN=false
+# execute 'Syncing pacman repository' $PACMAN -Syy
 
 export TMPDIR=$(mktemp -d)
 set_on_error_trap "rm -rf ${TMPDIR}"
-mkdir -p ${TMPDIR}/${CHOST}-toolchain/bin
-for tool in strip readelf objcopy; do
-    ln -s $(which ${CHOST}-$tool) ${TMPDIR}/${CHOST}-toolchain/bin/$tool
-done
-export PATH=${TMPDIR}/${CHOST}-toolchain/bin:"$PATH"
+# mkdir -p ${TMPDIR}/${CHOST}-toolchain/bin
+# for tool in strip readelf objcopy; do
+#     ln -s $(which ${CHOST}-$tool) ${TMPDIR}/${CHOST}-toolchain/bin/$tool
+# done
+# export PATH=${TMPDIR}/${CHOST}-toolchain/bin:"$PATH"
 
 # Build
 message 'Building packages' "${packages[@]}"
@@ -43,10 +54,11 @@ message 'Building packages' "${packages[@]}"
 # execute 'Approving recipe quality' check_recipe_quality
 for package in "${packages[@]}"; do
     # execute 'Building binary' makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild
-    execute 'Building binary' makepkg --noconfirm --skippgpcheck --nocheck \
-       --syncdeps --rmdeps --cleanbuild --config $(pwd)/makepkg-mingw64.conf
+    execute 'Building binary' makepkg --noconfirm --skippgpcheck --nocheck --nodeps \
+       --cleanbuild --config "${MAKEPKG_CONF}"
     # execute 'Building source' makepkg --noconfirm --noprogressbar --skippgpcheck --allsource --config ~/makepkg-mingw64.conf
-    execute 'Installing' "$PACMAN" --noconfirm --upgrade *.pkg.tar.xz
+    # execute 'Installing' $PACMAN --noconfirm --upgrade *.pkg.tar.xz
+    execute 'Installing' tar xvf *.pkg.tar.xz ${MINGW_PREFIX#/} -C /
     deploy_enabled && mv "${package}"/*.pkg.tar.xz artifacts
     # deploy_enabled && mv "${package}"/*.src.tar.gz artifacts
     for package_arg in "$@"; do
