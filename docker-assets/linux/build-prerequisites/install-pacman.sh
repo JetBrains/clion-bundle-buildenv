a81a29819a6333a5f5e39c41b56e54fc5e029917
Eldar Abusalimov <eldar.abusalimov@jetbrains.com>
2017-10-19 02:33:29 +0300

    linux.Dockerfile: Configure pacman with proper CHOST

--- docker-assets/linux/build-prerequisites/install-pacman.sh
+++ docker-assets/linux/build-prerequisites/install-pacman.sh
@@ -2,6 +2,20 @@
 
 set -ex
 
+patch_config_sub() {
+	local filename="$1"
+	mv "${filename}"{,.orig}
+	cat > "${filename}" <<'EOF'
+#!/bin/bash
+if [[ $1 == "x86_64-redhat-linux" ]]; then
+	echo "x86_64-redhat-linux"
+	exit 0
+fi
+exec "$0.orig" "$@"
+EOF
+	chmod a+x "${filename}"
+}
+
 cd $(dirname ${BASH_SOURCE})
 
 mkdir pacman-build
@@ -9,7 +23,8 @@ pushd pacman-build
 
 curl https://sources.archlinux.org/other/pacman/pacman-5.0.2.tar.gz | tar xz
 cd pacman-5.0.2
-./configure
+patch_config_sub build-aux/config.sub
+./configure --{build,host}="x86_64-redhat-linux"
 make
 make install
 
